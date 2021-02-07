090e3109a7558d94f342d2a55efde51347be4b13
khadidiatou98 <64222872+khadidiatou98@users.noreply.github.com>
2021-02-07 22:26:18 -0100

    Add files via upload

---
+++ Liage3_2021/~AutoRecover.Classe.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionEtudiant.models
+{
+    public class Classe
+    {
+        //Attributs
+        private int id;
+        private string libelle;
+        private int nbreEtudiant;
+
+
+        //Constructeurs
+        //Surcharges
+        //Par Défaut
+        public Classe()
+        {
+
+        }
+        //Insertion
+
+        public Classe(string libelle, int nbreEtudiant)
+        {
+            this.Libelle = libelle;
+            this.NbreEtudiant = nbreEtudiant;
+        }
+
+        public Classe(int id)
+        {
+            this.id = id;
+        }
+
+
+        //Selection
+        public Classe(int id, string libelle, int nbreEtudiant)
+        {
+            this.Id = id;
+            this.Libelle = libelle;
+            this.NbreEtudiant = nbreEtudiant;
+        }
+
+        //Get & Set
+        public int Id { get => id; set => id = value; }
+        public string Libelle { get => libelle; set => libelle = value; }
+        public int NbreEtudiant { get => nbreEtudiant; set => nbreEtudiant = value; }
+
+        //ToString()
+        public override string ToString()
+        {
+            return libelle;
+        }
+
+    }
+}
---
+++ Liage3_2021/~AutoRecover.FrmMenu.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestionEtudiant
+{
+    public partial class FrmMenu : Form
+    {
+        public FrmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMenu_Load(object sender, EventArgs e)
+        {
+            LoadFormCreerClasse();
+        }
+
+        private void classeToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            OuvrirFenetreConnexion();
+        }
+
+        private void FrmMenu_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            OuvrirFenetreConnexion();
+        }
+        public void OuvrirFenetreConnexion()
+        {
+            FrmConnexion frmConnexion = new FrmConnexion();
+            frmConnexion.Show();
+            this.Hide();
+        }
+
+        private void creerClasseToolStripMenuItem_Click(object sender, EventArgs e) 
+        {
+            LoadFormCreerClasse();
+        }
+
+        private void LoadFormCreerClasse()
+        {
+            EffacerFormFils();
+            Form1 frmCreerClasse = new Form1();
+            frmCreerClasse.MdiParent = this;
+            frmCreerClasse.Show();
+        }
+        private void EffacerFormFils()
+        {
+            foreach(Form form in this.MdiChildren)
+            {
+                form.Close();
+            }
+        }
+    }
+}
